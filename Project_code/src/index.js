6b67270b876d4fa968825ae1389158f73e3e2d36
Lucas Drager <ludr9377@colorado.edu>
2023-11-14 08:21:18 -0700

    Adding login route and various fixes to DB and WEB

--- Project_code/src/index.js
+++ Project_code/src/index.js
@@ -7,7 +7,7 @@ const app = express();
 const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
 const bodyParser = require('body-parser');
 const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
-//const bcrypt = require('bcrypt'); //  To hash passwords
+const bcrypt = require('bcrypt'); //  To hash passwords
 
 // *****************************************************
 // <!-- Section 2 : Connect to DB -->
@@ -38,49 +38,6 @@ db.connect()
 // <!-- Section 3 : App Settings -->
 // *****************************************************
 
-//Login Get call
-app.get("/login", (req,res) => {
-  res.render("pages/login.ejs")
-});
-
-//Login post call
-app.post("/login", async (req,res) => {
-  usernameQuery = `SELECT password FROM users WHERE users.username = $1`
-  var password = ""
-  await db.one(usernameQuery,[req.body.username])
-  .then((data) => {
-    password = data.password
-  })
-  .catch((err) => {
-    console.log(err)
-  });
-  const match = await bcrypt.compare(req.body.password, password);
-  console.log(match)
-  //save user details in session like in lab 8
-  if(match == false){ 
-    console.log('error')
-  } else {
-    req.session.user = req.body.username;
-    req.session.save();
-    res.redirect("/homepage")
-  }
-});
-
-//log out get call
-app.get("/logout", (req, res) => {
-  req.session.destroy();
-  res.render("pages/login.ejs");
-});
-
-app.get('/welcome', (req, res) => {
-  res.json({status: 'success', message: 'Welcome!'});
-});
-
-
-// Authentication and security
-app.set('view engine', 'ejs'); // set the view engine to EJS
-app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
-
 // initialize session variables
 app.use(
   session({
@@ -101,8 +58,87 @@ app.use(
 // <!-- Section 4 : API Routes -->
 // *****************************************************
 app.get("/", (req, res) => {
-    res.render("pages/homepage.ejs"); //this will call the /anotherRoute route in the API
+  res.redirect("/login"); //sent user to log in page
+});
+
+//Login Get call
+app.get("/login", (req,res) => {
+  res.render("pages/login.ejs")
+});
+
+//Login post call
+app.post("/login", async (req,res) => {
+  usernameQuery = `SELECT password FROM users WHERE users.username = $1`
+  var password = ""
+  await db.one(usernameQuery,[req.body.username])
+  .then((data) => {
+    password = data.password
+  })
+  .catch((err) => {
+    console.log(err)
+    res.redirect("/login")
   });
+  const match = await bcrypt.compare(req.body.password, password);
+  console.log(match)
+  //save user details in session like in lab 8
+  if(match == false){ 
+    console.log('error')
+    res.redirect("/login")
+  } else {
+    req.session.user = req.body.username;
+    req.session.save();
+    res.redirect("/homepage")
+  }
+});
+
+//log out get call
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.render("pages/login.ejs");
+});
+
+//Register get call
+app.get("/register", (req, res) => {
+  res.render("pages/register.ejs")
+});
+
+//Register post call
+app.post("/register", async (req, res) => {
+  //hash the password using bcrypt library //{status: 200, message: 'Failure'}
+  let hash;
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(req.body.password, salt, function(err, passHash) {
+      hash = passHash
+      if (err) { 
+        res.render("pages/register.ejs")
+      } else { 
+        console.log('fetched response');
+        const query = "INSERT INTO users (username, password) VALUES ($1,$2);";
+        db.any(query,[req.body.username,hash])
+        .then((data) => {
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.redirect("/register");
+        });
+      }
+    });
+  });
+});
+
+// Lab 11 test call
+app.get('/welcome', (req, res) => {
+  res.json({status: 'success', message: 'Welcome!'});
+});
+
+app.get("/homepage", (req, res) => {
+  res.render("pages/homepage.ejs")
+});
+
+// Authentication and security
+app.set('view engine', 'ejs'); // set the view engine to EJS
+app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
   
 // *****************************************************
 // <!-- Section 5 : Start Server-->
--- Project_code/src/init_data/create.sql
+++ Project_code/src/init_data/create.sql
@@ -1,61 +1,61 @@
 -- Create the users table
 CREATE TABLE IF NOT EXISTS users (
   username varchar(50) PRIMARY KEY,
-  password varchar(50),
+  password varchar(256),
   first_name varchar(50),
   last_name varchar(50),
   email varchar(50),
   created_at timestamp
 );
 
--- -- Create the friends table
--- CREATE TABLE IF NOT EXISTS friends (
---   user_a varchar(50),
---   user_b varchar(50),
---   FOREIGN KEY (user_a) REFERENCES users(username),
---   FOREIGN KEY (user_b) REFERENCES users(username)
--- );
+-- Create the friends table
+CREATE TABLE IF NOT EXISTS friends (
+  user_a varchar(50),
+  user_b varchar(50),
+  FOREIGN KEY (user_a) REFERENCES users(username),
+  FOREIGN KEY (user_b) REFERENCES users(username)
+);
 
--- -- Create the trip table
--- CREATE TABLE IF NOT EXISTS trip (
---   tripID serial PRIMARY KEY,
---   driverID varchar(50) REFERENCES users(username),
---   destination varchar(255),
---   original_location varchar(255)
--- );
+-- Create the trip table
+CREATE TABLE IF NOT EXISTS trip (
+  trip_id serial PRIMARY KEY,
+  driverID varchar(50) REFERENCES users(username),
+  destination varchar(255),
+  original_location varchar(255)
+);
 
--- -- Create the passengers table
--- CREATE TABLE IF NOT EXISTS passengers (
---   tripID integer,
---   passenger varchar(50) REFERENCES users(username),
---   FOREIGN KEY (tripID) REFERENCES trip(tripID)
--- );
+-- Create the passengers table
+CREATE TABLE IF NOT EXISTS passengers (
+  trip_id integer,
+  passenger varchar(50) REFERENCES users(username),
+  FOREIGN KEY (trip_id) REFERENCES trip(trip_id)
+);
 
--- -- Create the messaging table
--- CREATE TABLE IF NOT EXISTS messaging (
---   message_id serial PRIMARY KEY,
---   sender_id integer REFERENCES users(id),
---   receiver_id integer REFERENCES users(id),
---   message_text text,
---   sent_at timestamp
--- );
+-- Create the messaging table
+CREATE TABLE IF NOT EXISTS messaging (
+  message_id serial PRIMARY KEY,
+  sender_id varchar(50) REFERENCES users(username),
+  receiver_id varchar(50) REFERENCES users(username),
+  message_text text,
+  sent_at timestamp
+);
 
--- -- Create the ratings table
--- CREATE TABLE IF NOT EXISTS ratings (
---   rating_id serial PRIMARY KEY,
---   rater_id integer REFERENCES users(id),
---   ratee_id integer REFERENCES users(id),
---   rating_value real, 
---   rated_at timestamp,
---   review text
--- );
+-- Create the ratings table
+CREATE TABLE IF NOT EXISTS ratings (
+  rating_id serial PRIMARY KEY,
+  rater_id varchar(50) REFERENCES users(username),
+  ratee_id varchar(50) REFERENCES users(username),
+  rating_value real, 
+  rated_at timestamp,
+  review text
+);
 
--- -- Create the transactions table
--- CREATE TABLE IF NOT EXISTS transactions (
---   transaction_id serial PRIMARY KEY,
---   sender_id integer REFERENCES users(id),
---   receiver_id integer REFERENCES users(id),
---   amount numeric(10, 2), 
---   description varchar(255),
---   transaction_date timestamp
--- );
+-- Create the transactions table
+CREATE TABLE IF NOT EXISTS transactions (
+  transaction_id serial PRIMARY KEY,
+  sender_id varchar(50) REFERENCES users(username),
+  receiver_id varchar(50) REFERENCES users(username),
+  amount numeric(10, 2), 
+  description varchar(255),
+  transaction_date timestamp
+);
